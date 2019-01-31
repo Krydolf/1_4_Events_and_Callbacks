6a5962cbd22509cfa1d30f95b9c2f15543a7db2c
krydolf <peter.brandin@afconsult.com>
2019-01-31 13:04:43 +0100

    Add project files.

---
+++ 1_4_Events_and_Callbacks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_4_Events_and_Callbacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
